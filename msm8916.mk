eced936f3bf168d01daf6229bf4e77c9ee901ec9
Eloi Carracedo Muns <eloimuns@gmail.com>
2019-03-25 20:35:45 +0100

    Revert "msm8916-common: Build custom QTI telephony extension"
    This reverts commit 00b53bffb9a18584464c4d76e18cab7eeb9d1e2f.
    
    It's like just removes icon, sim is still active.
    
    Change-Id: Ifdbba6c453c2d9d28cc75942f5cca86ba6a653b5

--- msm8916.mk
+++ msm8916.mk
@@ -231,14 +231,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
-# Telephony
-PRODUCT_PACKAGES += \
-    qti-telephony-common \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
